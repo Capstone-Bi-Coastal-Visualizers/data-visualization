6be92f2767c6a4eb84877bc29ca4eb6d91bd4594
Freddie Bergener <freddiembergener@gmail.com>
2021-07-01 12:36:18 -0400

    Feat: Added react-chartjs-2, airports.json, and example code for stacked bar chart

--- client/App.js
+++ client/App.js
@@ -1,7 +1,6 @@
-import React from 'react'
-
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
 
 const App = () => {
   return (
@@ -9,7 +8,61 @@ const App = () => {
       <Navbar />
       <Routes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+// -------------------------------------------------
+// Example code for stacked bar chart
+// import React from "react";
+// import { Bar } from "react-chartjs-2";
+
+// const arbitraryStackKey = "stack1";
+
+// const data = {
+//   labels: ["a", "b", "c", "d", "e"],
+//   datasets: [
+//     // These two will be in the same stack.
+//     {
+//       stack: arbitraryStackKey,
+//       label: "data1",
+//       backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+//       data: [1, 2, 3, 4, 5],
+//     },
+//     {
+//       stack: arbitraryStackKey,
+//       label: "data2",
+//       backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(255, 99, 132, 0.2)"],
+//       data: [5, 4, 3, 2, 1],
+//     },
+//   ],
+// };
+
+// const options = {
+//   scales: {
+//     yAxes: [
+//       {
+//         stacked: true,
+//         ticks: {
+//           beginAtZero: true,
+//         },
+//       },
+//     ],
+//     xAxes: [
+//       {
+//         stacked: true,
+//       },
+//     ],
+//   },
+// };
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Bar data={data} options={options} />
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
