3f40ef131f2e6fb8ff3f830e6835abc09a505ba3
Freddie Bergener <freddiembergener@gmail.com>
2021-07-05 13:35:22 -0400

    Feat: Rendered charts with dummy data

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 import SearchBar from "./components/SearchBar";
+import BarChart from "./components/BarChart";
 
 const App = () => {
   return (
@@ -9,6 +10,17 @@ const App = () => {
       <Navbar />
       <Routes />
       <SearchBar />
+      <BarChart
+        data={{
+          flightOne: 120.51,
+          flightTwo: 140.0,
+          hotelOne: 120.58,
+          hotelTwo: 180,
+          budget: 300,
+          destinationOne: "Los Angeles",
+          destinationTwo: "New York",
+        }}
+      />
     </div>
   );
 };
