cd00525346f03ca2d2f95a1006c0db431fb71571
Freddie Bergener <freddiembergener@gmail.com>
2021-07-06 13:00:20 -0400

    Chore: Set up Search Result component and route

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import SearchBar from "./SearchBar";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+export const Home = (props) => {
+  // const { username } = props;
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      {/* <h3>Welcome{, {username}}</h3> */}
+      <h2>Welcome</h2>
+      <SearchBar />
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
