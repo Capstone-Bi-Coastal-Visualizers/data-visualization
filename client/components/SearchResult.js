0faf453a3ea19642f59e3c6f90dfb4450dacef84
Freddie Bergener <freddiembergener@gmail.com>
2021-07-08 13:00:44 -0400

    Feat: user can now select a trip and see a bar graph with trip details on the confirmation page

--- client/components/SearchResult.js
+++ client/components/SearchResult.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Bar } from "react-chartjs-2";
 import { Link } from "react-router-dom";
+import { setTrip } from "../store/tripData";
 
 const SearchResult = () => {
   const tripData = useSelector((state) => state.tripDataReducer);
+  const dispatch = useDispatch();
   const {
     tripOneFirstFlight,
     tripOneReturningFlight,
@@ -17,7 +19,7 @@ const SearchResult = () => {
     tripTwoHotelData,
     tripTwoStayDuration,
   } = tripData;
-  if (Object.keys(tripData).length === 9) {
+  if (Object.keys(tripData).length >= 9) {
     const tripOneFlights =
       tripOneFirstFlight[1].MinPrice + tripOneReturningFlight[1].MinPrice;
     const tripTwoFlights =
@@ -87,8 +89,14 @@ const SearchResult = () => {
     return (
       <div>
         <Bar data={data} options={options} />
-        <Link to="/confirmation-page"> Select Trip One </Link>
-        <Link to="/confirmation-page"> Select Trip Two </Link>
+        <Link to="/confirmation-page" onClick={() => dispatch(setTrip(1))}>
+          {" "}
+          Select Trip One{" "}
+        </Link>
+        <Link to="/confirmation-page" onClick={() => dispatch(setTrip(2))}>
+          {" "}
+          Select Trip Two{" "}
+        </Link>
 
         <h2>{destinationOne}</h2>
         <h3>
