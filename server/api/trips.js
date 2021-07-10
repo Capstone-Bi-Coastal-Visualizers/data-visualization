8bf8479187a0f600e359dc595e0839cf82e43fa0
Joseph Stalnaker <josephlstalnaker@gmail.com>
2021-07-10 12:31:42 -0400

    Chore: destructure the implementation of isUser

--- server/api/trips.js
+++ server/api/trips.js
@@ -3,21 +3,9 @@ const {
   models: { Trip, User },
 } = require("../db");
 
-module.exports = router;
+const isUser = require("./isUser")
 
-const isUser = async (req, res, next) => {
-    try {
-      const user = await User.findByToken(req.headers.authorization);
-      req.user = user;
-      if (user.id) {
-        next();
-      } else {
-        res.sendStatus(403);
-      }
-    } catch (error) {
-      next(error);
-    }
-  };
+module.exports = router;
 
 router.get("/", isUser, async (req, res, next) => {
     try {
