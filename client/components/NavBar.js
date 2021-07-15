dfe09f0ba88d218266c04fd7ffe576a88bebb75b
Joseph Stalnaker <josephlstalnaker@gmail.com>
2021-07-15 14:47:33 -0400

    Chore: updated final CSS

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -20,46 +20,41 @@ export default function NavBar() {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="container-logo">
-      <div className="image logo-container is-clickable" onClick={() => {handleNav("/")}}>
-      </div>
-      </div>
-      <div className="navbar-brand">
-        <a
+        <div
+          className="image logo-container is-clickable"
           onClick={() => {
-            setisActive(!isActive);
+            handleNav("/");
           }}
-          role="button"
-          className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
+        ></div>
       </div>
       <div className="navbar-end">
         <div className="navbar-button-container">
-            {isLoggedIn ? (
-              <div className="button-container">
-                <a className="button is-primary"  onClick={() => {handleNav("/trips")}}>Trips</a>
-                <a className="button is-primary" href="#" onClick={handleClick}>
-                  Logout
-                </a>
-              </div>
-            ) : (
-              <div className="button-container">
-                <a onClick={() => handleToggle("Login")}>
-                  <div className="button is-primary">Login</div>
-                </a>
-                <a onClick={() => handleToggle("Signup")}>
-                  <div className="button is-primary">Sign Up</div>
-                </a>
-              </div>
-            )}
-          </div>
+          {isLoggedIn ? (
+            <div className="button-container">
+              <a
+                className="button is-primary"
+                onClick={() => {
+                  handleNav("/trips");
+                }}
+              >
+                Trips
+              </a>
+              <a className="button is-primary" href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div className="button-container">
+              <a onClick={() => handleToggle("Login")}>
+                <div className="button is-primary">Login</div>
+              </a>
+              <a onClick={() => handleToggle("Signup")}>
+                <div className="button is-primary">Sign Up</div>
+              </a>
+            </div>
+          )}
         </div>
+      </div>
     </nav>
   );
 }
