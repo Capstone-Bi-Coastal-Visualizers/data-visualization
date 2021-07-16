8f1e53177c765b12a3844df9a2c4756a5e1879ec
Joseph Stalnaker <josephlstalnaker@gmail.com>
2021-07-16 10:50:05 -0400

    Chore: completed random background render and favicon updated

--- client/App.js
+++ client/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Routes from "./Routes";
 import NavBar from "./components/NavBar"
 
+const num = Math.floor(Math.random() * 10) + 1 
 const App = () => {
   return (
     <div style={{ 
       backgroundSize: 'cover',
             overflow: 'hidden',
-      backgroundImage: `url("images/5.png")` 
+      // backgroundImage: `url("images/5.png")`, 
+      backgroundImage: `url("images/${num}.png")` 
     }}>
     <div>
       <NavBar />
